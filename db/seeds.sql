5d2acc2284cadbb621b37800271e463cd5f08af3
varan2030 <varan2030@mail.ru>
2018-03-10 00:06:33 -0500

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Royal burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Organic burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Big burger', false);
